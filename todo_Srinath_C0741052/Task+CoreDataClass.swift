733775277610525f8a3abd42e0e91bbcf68f18be
Srinath <srinathsuddala@gmail.com>
2020-06-27 19:40:09 +0530

    Added category fetch request

--- todo_Srinath_C0741052/Task+CoreDataClass.swift
+++ todo_Srinath_C0741052/Task+CoreDataClass.swift
@@ -3,5 +3,11 @@ import CoreData
 
 @objc(Task)
 public class Task: NSManagedObject {
-
+    @nonobjc public class func fetchRequest(with categoryUuid: UUID) -> NSFetchRequest<Task> {
+        let fetch = NSFetchRequest<Task>(entityName: "Task")
+        fetch.predicate = NSPredicate(format: "category.uuid == %@", argumentArray: [categoryUuid])
+        fetch.sortDescriptors = [NSSortDescriptor(key: "dateCreated", ascending: false)]
+        
+        return fetch
+    }
 }
