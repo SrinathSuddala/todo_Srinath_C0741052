ae5e552a1c4b7da5baaa7c0ae630f093c9f05b56
Srinath <srinathsuddala@gmail.com>
2020-06-27 19:52:59 +0530

    Added filter fetch request

--- todo_Srinath_C0741052/Task+CoreDataClass.swift
+++ todo_Srinath_C0741052/Task+CoreDataClass.swift
@@ -10,4 +10,12 @@ public class Task: NSManagedObject {
         
         return fetch
     }
+    
+    @nonobjc public class func fetchRequest(with searchText: String, categoryUuid: UUID) -> NSFetchRequest<Task> {
+        let fetch = NSFetchRequest<Task>(entityName: "Task")
+        fetch.predicate = NSPredicate(format: "(title contains[c] %@ OR desc contains[c] %@) AND category.uuid == %@", argumentArray: [searchText, searchText, categoryUuid])
+        fetch.sortDescriptors = [NSSortDescriptor(key: "dateCreated", ascending: false)]
+        
+        return fetch
+    }
 }
