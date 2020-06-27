d277b1b2e1cf34b78a95863621b6d8c8c82b634c
Srinath <srinathsuddala@gmail.com>
2020-06-27 13:43:35 +0530

    designed category details view

--- todo_Srinath_C0741052/todo_Srinath_C0741052/CategoryDetailViewController.swift
+++ todo_Srinath_C0741052/todo_Srinath_C0741052/CategoryDetailViewController.swift
@@ -2,13 +2,19 @@
 import UIKit
 
 class CategoryDetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField : UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Category details"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(saveTapped))
         // Do any additional setup after loading the view.
     }
     
+    @objc func saveTapped() {
+        
+    }
 
     /*
     // MARK: - Navigation
