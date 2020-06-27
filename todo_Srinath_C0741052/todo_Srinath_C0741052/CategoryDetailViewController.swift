561c3a5df34005bc8cdb4f71411c7cc070bbdb5e
Srinath <srinathsuddala@gmail.com>
2020-06-27 13:44:31 +0530

    Saved category on save button tap

--- todo_Srinath_C0741052/todo_Srinath_C0741052/CategoryDetailViewController.swift
+++ todo_Srinath_C0741052/todo_Srinath_C0741052/CategoryDetailViewController.swift
@@ -4,16 +4,23 @@ import UIKit
 class CategoryDetailViewController: UIViewController {
     
     @IBOutlet weak var titleTextField : UITextField!
+    let appdelegate = UIApplication.shared.delegate as! AppDelegate
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Category details"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(saveTapped))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(saveTapped))
         // Do any additional setup after loading the view.
     }
     
     @objc func saveTapped() {
-        
+        guard let title = titleTextField.text, !title.isEmpty else { return }
+        let category = Category(context: appdelegate.persistentContainer.viewContext)
+        category.title = title
+        category.uuid = UUID()
+        appdelegate.persistentContainer.viewContext.insert(category)
+        try? appdelegate.persistentContainer.viewContext.save()
+        self.navigationController?.popViewController(animated: true)
     }
 
     /*
