2800d266f5874034b34e6e1ea55047fd2761cd53
Srinath <srinathsuddala@gmail.com>
2020-06-27 19:44:14 +0530

    Loaded tasks on view will appear

--- todo_Srinath_C0741052/todo_Srinath_C0741052/TaskListViewController.swift
+++ todo_Srinath_C0741052/todo_Srinath_C0741052/TaskListViewController.swift
@@ -1,13 +1,14 @@
 
 
 import UIKit
+import CoreData
 
 class TaskListViewController: UIViewController {
     
     @IBOutlet weak var taskListTableView: UITableView!
     var tasks: [Task] = []
     var selectedCategory: Category!
-    
+    let appdelegate = UIApplication.shared.delegate as! AppDelegate
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,10 +19,24 @@ class TaskListViewController: UIViewController {
         // Do a ny additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tasks = fetchTasks()
+        taskListTableView.reloadData()
+    }
+    
+    func fetchTasks() -> [Task] {
+        guard let tasks = try? appdelegate.persistentContainer.viewContext.fetch(Task.fetchRequest(with: selectedCategory.uuid!) as NSFetchRequest<Task>) else {
+            return []
+        }
+        return tasks
+    }
+    
 
     @objc func addTapped() {
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         let viewController = storyboard.instantiateViewController(withIdentifier: "TaskDetailsViewController") as! TaskDetailsViewController
+        viewController.selectedCategory = selectedCategory
         self.navigationController?.pushViewController(viewController, animated: true)
     }
     
