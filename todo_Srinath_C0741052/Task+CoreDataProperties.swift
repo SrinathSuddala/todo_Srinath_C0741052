a9bb403af7c76984161ef937a5d29ba0d351df2c
Srinath <srinathsuddala@gmail.com>
2020-06-27 13:31:45 +0530

    Added task model and realationship

---
+++ todo_Srinath_C0741052/Task+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+import Foundation
+import CoreData
+
+
+extension Task {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Task> {
+        return NSFetchRequest<Task>(entityName: "Task")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var desc: String?
+    @NSManaged public var dateCreated: Date?
+    @NSManaged public var dueDate: Date?
+    @NSManaged public var numberOfDays: Int64
+    @NSManaged public var isExpired: Bool
+    @NSManaged public var category: Category?
+
+}
