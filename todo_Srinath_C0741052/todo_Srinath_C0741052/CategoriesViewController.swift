bdb4cc581f41da2ead2c5640cc4e0a641cf64996
Srinath <srinathsuddala@gmail.com>
2020-06-27 13:38:33 +0530

    Added category list table view data

--- todo_Srinath_C0741052/todo_Srinath_C0741052/CategoriesViewController.swift
+++ todo_Srinath_C0741052/todo_Srinath_C0741052/CategoriesViewController.swift
@@ -4,13 +4,41 @@ import UIKit
 class CategoriesViewController: UIViewController {
     
     @IBOutlet weak var categoryTable: UITableView!
+    var categories: [Category] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Categories"
+        
+        categoryTable.register(UITableViewCell.self,
+                               forCellReuseIdentifier: "Cell")
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addTapped))
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @objc func addTapped() {
+        
+    }
 }
 
+
+extension CategoriesViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView,
+                 numberOfRowsInSection section: Int) -> Int {
+    
+    return categories.count
+  }
+  
+  func tableView(_ tableView: UITableView,
+                 cellForRowAt indexPath: IndexPath)
+    -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        let category = categories[indexPath.row]
+        cell.textLabel?.text = category.title
+        return cell
+    }
+}
