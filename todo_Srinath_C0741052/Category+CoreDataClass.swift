fca2734766aeef689230e165b8c40cfe173a1165
Srinath <srinathsuddala@gmail.com>
2020-06-29 18:38:34 +0530

    Moved deleted category to archive category

--- todo_Srinath_C0741052/Category+CoreDataClass.swift
+++ todo_Srinath_C0741052/Category+CoreDataClass.swift
@@ -3,5 +3,12 @@ import CoreData
 
 @objc(Category)
 public class Category: NSManagedObject {
+    
+    @nonobjc public class func fetchRequest(with title: String) -> NSFetchRequest<Category> {
+        let fetch = NSFetchRequest<Category>(entityName: "Category")
+        fetch.predicate = NSPredicate(format: "title == %@", argumentArray: [title])
+        
+        return fetch
+    }
 
 }
--- todo_Srinath_C0741052/todo_Srinath_C0741052/TaskListViewController.swift
+++ todo_Srinath_C0741052/todo_Srinath_C0741052/TaskListViewController.swift
@@ -10,12 +10,16 @@ class TaskListViewController: UIViewController {
     var selectedCategory: Category!
     let appdelegate = UIApplication.shared.delegate as! AppDelegate
 
+    var isArchivedCategory: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Tasks"
+        isArchivedCategory = selectedCategory.title == "Archived"
         taskListTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addTapped))
-        
+        if isArchivedCategory {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addTapped))
+        }
         // Do a ny additional setup after loading the view.
     }
     
@@ -62,7 +66,7 @@ extension TaskListViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
+        return !isArchivedCategory
     }
     
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
@@ -101,14 +105,32 @@ extension TaskListViewController: UITableViewDelegate {
     func deleteTask(at indexPath: IndexPath) {
         taskListTableView.beginUpdates()
         let task = tasks[indexPath.row]
+        move(task: task)
         tasks.remove(at: indexPath.row)
         taskListTableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
-        appdelegate.persistentContainer.viewContext.delete(task)
         taskListTableView.endUpdates()
+    }
+    
+    func move(task: Task) {
+        if let categories = try? appdelegate.persistentContainer.viewContext.fetch(Category.fetchRequest(with: "Archived") as NSFetchRequest<Category>), categories.count > 0  {
+            task.category = categories[0]
+            try? appdelegate.persistentContainer.viewContext.save()
+        } else {
+            let category = Category(context: appdelegate.persistentContainer.viewContext)
+            category.title = "Archived"
+            category.uuid = UUID()
+            appdelegate.persistentContainer.viewContext.insert(category)
+            task.category = category
+            try? appdelegate.persistentContainer.viewContext.save()
+            
+        }
     }
     
     func color(for task: Task) -> UIColor {
         guard let currentDate = task.dateCreated else { return .clear }
+        if isArchivedCategory {
+            return .clear
+        }
         var dateComponent = DateComponents()
         dateComponent.day = Int(task.numberOfDays)
         let futureDate = Calendar.current.date(byAdding: dateComponent, to: currentDate)!
